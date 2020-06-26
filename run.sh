c77e2e218ca36ed2758d6f61e392d099cffdb84f
soyko <oleg@debian-physical.soyko.net>
2020-06-26 16:27:58 -0400

    Committer: soyko <oleg@debian-physical.soyko.net>
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   run.sh
    	Added speedtest with flags to run after install.

--- run.sh
+++ run.sh
@@ -4,9 +4,11 @@
 
 # Download speedtest.net Linux 32bit tar gip file and save it as speedtest.tgz in current directory.
 wget -O speedtest.tgz https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-1.0.0-i386-linux.tgz
-#Extract the  speedtest.tgz file into local directory.
+# Extract the  speedtest.tgz file into local directory.
 tar zxvf speedtest.tgz
 # Make /etc/ssl/certs folder as root on the install.
 sudo mkdir -p /etc/ssl/certs
 # Download ssl certificates so speedtest.net app can run, saving the file as ca-certificates.crt inside of the above created folder.
 sudo wget -O /etc/ssl/certs/ca-certificates.crt https://curl.haxx.se/ca/cacert.pem
+# Run the speedtest CLI app with verbose output stating what server was selected and what the latency to each was in testing.
+./speedtest --selection-details
