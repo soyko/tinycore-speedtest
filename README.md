e38e66a6218e962077408eab0c6f79071c30a0b9
Oleg <oleg@debian-physical.soyko.net>
2020-06-25 20:59:46 -0400

    Committer: Oleg <oleg@debian-physical.soyko.net>
     On branch master
    
     Changes to be committed:
    	modified:   README.md
    	new file:   run.sh

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # tinycore-speedtest
 Speedtest.net setup in base TinyCore
+
+There are times when I need to do something inside of a base tinycore instance for work where I don't have speedtest available.
+I'm told to user iperf, but it's not the best.
+
+Made this so I can just have a one stop shop to use this as needed.
---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Oleg Soyko
+# 25/06/2020
+
+# Download speedtest.net Linux 32bit tar gip file and save it as speedtest.tgz in current directory.
+wget -O speedtest.tgz https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-1.0.0-i386-linux.tgz
+#Extract the  speedtest.tgz file into local directory.
+tar zxvf speedtest.tgz
+# Make /etc/ssl/certs folder as root on the install.
+sudo mkdir -p /etc/ssl/certs
+# Download ssl certificates so speedtest.net app can run, saving the file as ca-certificates.crt inside of the above created folder.
+wget -O /etc/ssl/certs/ca-certificates.crt https://curl.haxx.se/ca/cacert.pem
